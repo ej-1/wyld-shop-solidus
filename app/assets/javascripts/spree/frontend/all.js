d86d44879e81c822daea224341161190dc9ba659
Erik <erikwjonsson@gmail.com>
2017-01-30 19:29:51 +0100

    Added template files for Neuer template. Works good enogh to commit. Some assets are missing making the load time slow. HTML code for template needs to be broken up and put in the right views.

---
+++ app/assets/javascripts/spree/frontend/all.js
@@ -0,0 +1,102 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+
+!function (a) {
+    "use strict";
+    /*Mobile menu selected to close menu*/
+    $("#bs-example-navbar-collapse-1 ul li").click(function () {
+        $("#bs-example-navbar-collapse-1").removeClass("in");
+    });
+
+    $("#js-grid-juicy-projects").on('initComplete.cbp', function() {
+    	$(".cbp-l-loadMore-button").css({"visibility": "visible", "opacity": "1", "transition-delay": "0s"});
+	});
+
+
+}(this);
+
+
+(function($, window, document, undefined) {
+    'use strict';
+
+    $('#js-grid-juicy-projects').cubeportfolio({
+        filters: '.js-filters-juicy-projects',
+        loadMore: '#js-loadMore-juicy-projects',
+        loadMoreAction: 'click',
+        layoutMode: 'grid',
+        defaultFilter: '*',
+        animationType: 'quicksand',
+        gapHorizontal: 0,
+        gapVertical: 0,
+        gridAdjustment: 'responsive',
+        mediaQueries: [{
+            width: 1600,
+            cols: 5
+        }, {
+            width: 1100,
+            cols: 4
+        }, {
+            width: 800,
+            cols: 3
+        }, {
+            width: 480,
+            cols: 2
+        }, {
+            width: 320,
+            cols: 1
+        }],
+        caption: 'overlayBottomReveal',
+        displayType: 'sequentially',
+        displayTypeSpeed: 80,
+
+        // lightbox
+        lightboxDelegate: '.cbp-lightbox',
+        lightboxGallery: true,
+        lightboxTitleSrc: 'data-title',
+        lightboxCounter: '<div class="cbp-popup-lightbox-counter">{{current}} of {{total}}</div>',
+
+        // singlePage popup
+        singlePageDelegate: '.cbp-singlePage',
+        singlePageDeeplinking: true,
+        singlePageStickyNavigation: false,
+        singlePageCounter: '<div class="cbp-popup-singlePage-counter">{{current}} of {{total}}</div>',
+        singlePageCallback: function(url, element) {
+            // to update singlePage content use the following method: this.updateSinglePage(yourContent)
+            var t = this;
+
+            $.ajax({
+                    url: url,
+                    type: 'GET',
+                    dataType: 'html',
+                    timeout: 30000
+                })
+                .done(function(result) {
+                    t.updateSinglePage(result);
+                })
+                .fail(function() {
+                    t.updateSinglePage('AJAX Error! Please refresh the page!');
+                });
+        },
+    });
+    
+})(jQuery, window, document);
+
+});
