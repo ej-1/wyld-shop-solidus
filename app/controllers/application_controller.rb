d55a55ad421b6785df34bb8ab3ae82f828daca2e
Erik <erikwjonsson@gmail.com>
2017-02-07 23:35:44 +0100

    BIG COMMIT: When user logs in on product page via modal, the user is redirected back to the product page and not landing page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
+
   protect_from_forgery with: :exception
+  def after_sign_in_path_for(resource) # Custom method to meant to redirect user back to product page after login.
+    request.env['omniauth.origin'] || stored_location_for(resource) || (params["current_url"].nil? == true) ? '/' : params["current_url"] # https://github.com/plataformatec/devise/wiki/How-To:-redirect-to-a-specific-page-on-successful-sign-in
+    #binding.pry
+  end
 end
+
---
+++ app/controllers/spree/user_sessions_controller.rb
@@ -0,0 +1,75 @@
+class Spree::UserSessionsController < Devise::SessionsController
+  helper 'spree/base', 'spree/store'
+  if Spree::Auth::Engine.dash_available?
+    helper 'spree/analytics'
+  end
+
+  include Spree::Core::ControllerHelpers::Auth
+  include Spree::Core::ControllerHelpers::Common
+  include Spree::Core::ControllerHelpers::Order
+  include Spree::Core::ControllerHelpers::Store
+
+  # This is included in ControllerHelpers::Order.  We just want to call
+  # it after someone has successfully logged in.
+  after_action :set_current_order, only: :create
+
+  def create
+    authenticate_spree_user!
+
+    if spree_user_signed_in?
+      respond_to do |format|
+        format.html do
+          flash[:success] = Spree.t(:logged_in_succesfully)
+          redirect_back_or_default(after_sign_in_path_for(spree_current_user))
+        end
+        #format.js { render success_json }
+      end
+    else
+      if request.xhr? # Checks if AJAX - if AJAX then the request comes from the modal
+        @product_url = params['current_url']
+        respond_to do |format|
+          flash.now[:error] = t('devise.failure.invalid')
+          format.js { render :layout => false }
+          #format.js do
+          #  render json: { error: t('devise.failure.invalid') },
+          #    status: :unprocessable_entity
+          #end
+        end
+      else
+        respond_to do |format|
+          format.html do
+            flash.now[:error] = t('devise.failure.invalid')
+            render :new
+          end
+        end
+      end
+    end
+  end
+
+  protected
+
+  def translation_scope
+    'devise.user_sessions'
+  end
+
+  private
+
+  def accurate_title
+    Spree.t(:login)
+  end
+
+  def redirect_back_or_default(default)
+    redirect_to(session["spree_user_return_to"] || default)
+    session["spree_user_return_to"] = nil
+  end
+
+  def success_json
+    {
+      json: {
+        user: spree_current_user,
+        ship_address: spree_current_user.ship_address,
+        bill_address: spree_current_user.bill_address
+      }.to_json
+    }
+  end
+end
